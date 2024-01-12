bb8638430b8c22a4bae3944b82b528bf3fa143f0
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-01-12 13:02:05 -0500

    update constraints

---
+++ constraints/htg_zrf_hh_timing.tcl
@@ -0,0 +1,51 @@
+######## CONVENIENCE FUNCTIONS
+
+proc set_cc_paths { srcClk dstClk ctlist } {
+    array set ctypes $ctlist
+    set srcType $ctypes($srcClk)
+    set dstType $ctypes($dstClk)
+    set maxTime [get_property PERIOD $srcClk]
+    set srcRegs [get_cells -hier -filter "CUSTOM_CC_SRC == $srcType"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_CC_DST == $dstType"]
+    set_max_delay -datapath_only -from $srcRegs -to $dstRegs $maxTime
+}
+
+proc set_gray_paths { srcClk dstClk ctlist } {
+    array set ctypes $ctlist
+    set maxTime [get_property PERIOD $srcClk]
+    set maxSkew [expr min([get_property PERIOD $srcClk], [get_property PERIOD $dstClk])]
+    set srcRegs [get_cells -hier -filter "CUSTOM_GRAY_SRC == $ctypes($srcClk)"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_GRAY_DST == $ctypes($dstClk)"]
+    set_max_delay -datapath_only -from $srcRegs -to $dstRegs $maxTime
+    set_bus_skew -from $srcRegs -to $dstRegs $maxSkew
+}
+
+proc set_ignore_paths { srcClk dstClk ctlist } {
+    array set ctypes $ctlist
+    set srcRegs [get_cells -hier -filter "CUSTOM_IGN_SRC == $ctypes($srcClk)"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_IGN_DST == $ctypes($dstClk)"]
+    set_false_path -from $srcRegs -to $dstRegs
+}
+
+######## END CONVENIENCE FUNCTIONS
+
+######## CLOCK DEFINITIONS
+
+#### PIN CLOCKS
+set refclk [create_clock -period 2.666 -name ref_clk [get_ports FPGA_REFCLK_IN_P]]
+set clktypes($refclk) REFCLK
+
+#### INTERNAL CLOCKS
+set psclk [get_clocks -of_objects [get_nets -hier -filter { NAME =~ "ps_clk" }]]
+set clktypes($psclk) PSCLK
+
+#### CONVENIENCE DEF
+# create the clktypelist variable to save
+set clktypelist [array get clktypes]
+
+# extra stuff
+set_property BITSTREAM.CONFIG.UNUSEDPIN PULLNONE [current_design]
+set_property C_CLK_INPUT_FREQ_HZ 300000000 [get_debug_cores dbg_hub]
+set_property C_ENABLE_CLK_DIVIDER false [get_debug_cores dbg_hub]
+set_property C_USER_SCAN_CHAIN 1 [get_debug_cores dbg_hub]
+set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
